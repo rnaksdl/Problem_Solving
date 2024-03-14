87a1fb34c318b0c14908c5c1015ea8b0fb537e08
Kyumin Lee <2km4890@gmail.com>
2024-03-13 21:01:47 -0500

    added 55.py and edited others description.

--- LeetCode/80.py
+++ LeetCode/80.py
@@ -1,14 +1,5 @@
 '''
 80. Remove Duplicates from Sorted Array II
-Medium
-
-6532
-
-1243
-
-Add to List
-
-Share
 Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
 
 Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
