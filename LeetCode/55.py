3c820430fdab8447dc671854a5f7245ada04bbd2
Kyumin Lee <2km4890@gmail.com>
2024-03-13 21:05:09 -0500

    added first attempt of 55

--- LeetCode/55.py
+++ LeetCode/55.py
@@ -5,4 +5,25 @@ You are given an integer array nums. You are initially positioned at the array's
 Return true if you can reach the last index, or false otherwise.
 '''
 
+from typing import List
+
+"""
+My first attempt was to actually jump through the array, but it was inefficient.
+"""
+class Solution1:
+    def canJump(self, nums: List[int]) -> bool:
+        i = 0
+        pos = 0 
+        
+        while pos < len(nums) - 1:
+            pos += nums[i]
+            i = pos
+            
+        if pos == len(nums) - 1:
+            return True
+        else: return False
+        
+solution = Solution1()
+print(solution.canJump([2,3,1,1,4]))
+
 
