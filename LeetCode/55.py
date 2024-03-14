b6acb9d67562948d2fa7bde34b08a4bc952ffa33
Kyumin Lee <2km4890@gmail.com>
2024-03-13 21:23:30 -0500

    edited sol1 and added sol2

--- LeetCode/55.py
+++ LeetCode/55.py
@@ -23,7 +23,24 @@ class Solution1:
             return True
         else: return False
         
-solution = Solution1()
-print(solution.canJump([2,3,1,1,4]))
+solution1 = Solution1()
+print(solution1.canJump([2,3,1,1,4]))
 
 
+"""
+More efficient way
+I learned about range(start, stop, step)
+This approach goes througth the array backwards and updates the goal if it's reachable.
+"""
+
+class Solution2:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        
+        for i in range(len(nums) - 1, -1 , -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return True if goal == 0 else False
+    
+solution2 = Solution2()
+print(solution2.canJump([2,3,1,1,4]))
