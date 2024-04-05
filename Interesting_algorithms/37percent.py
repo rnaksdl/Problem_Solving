18beab9fc02f5954256277a9c62180e661879e05
Kyumin Lee <2km4890@gmail.com>
2024-04-05 13:27:53 -0500

    metioned about the issue

--- Interesting_algorithms/37percent.py
+++ Interesting_algorithms/37percent.py
@@ -5,10 +5,13 @@ But it needs to know the length of the array
 You give it the length of the array,
 it will ask for the first 37% of the array
 then it will simply confirm that the value greater than the max of the first 37% of the array is the best bet.
+
+this method is really weirdly explained in a YouTube video.
+I'm now sure how I can implement "reject 37% just to get a sense of what's out there" part..
 '''
 
 
-from cmath import inf
+from cmath import infI
 from typing import List
 
 
