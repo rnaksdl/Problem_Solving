db3260e665cadccfb013ebbf93976fd342fd156e
Kyumin Lee <2km4890@gmail.com>
2024-04-05 13:00:38 -0500

    setting up 37 percent function

---
+++ Interesting_algorithms/37percent.py
@@ -0,0 +1,9 @@
+'''
+This method is used to find max value without evaluating all contents of the array.
+But it needs to know the length of the array
+
+You give it the length of the array,
+it will ask for the first 37% of the array
+then it will simply confirm that the value greater than the max of the first 37% of the array is the best bet.
+'''
+
