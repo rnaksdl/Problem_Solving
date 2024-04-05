1f65601c75c74654a310eb75a6615ab9126d5e5d
Kyumin Lee <2km4890@gmail.com>
2024-04-04 20:25:40 -0500

    setting up 58.py

---
+++ LeetCode/58.py
@@ -0,0 +1,9 @@
+'''
+58. Length of Last Word
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+'''
+
+# 8:06 start set up
+# 8:11 start solving
