5846db3dbe7b0202cf736be6d9399c1783e37b41
Kyumin Lee <2km4890@gmail.com>
2024-04-04 20:26:02 -0500

    first attempt

--- LeetCode/58.py
+++ LeetCode/58.py
@@ -7,3 +7,13 @@ A word is a maximal substring consisting of non-space characters only.
 
 # 8:06 start set up
 # 8:11 start solving
+
+class Solution1:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        for char in s:
+            if char == " ":
+                length = 0
+            else:
+                length += 1
+        return length
