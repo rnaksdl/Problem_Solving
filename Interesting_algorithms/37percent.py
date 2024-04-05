537d1591f60287f1964ff5888fee982a0158ed1c
Kyumin Lee <2km4890@gmail.com>
2024-04-05 13:24:51 -0500

    done but found issues

--- Interesting_algorithms/37percent.py
+++ Interesting_algorithms/37percent.py
@@ -7,3 +7,27 @@ it will ask for the first 37% of the array
 then it will simply confirm that the value greater than the max of the first 37% of the array is the best bet.
 '''
 
+
+from cmath import inf
+from typing import List
+
+
+class Solution:
+    def thirtyseven_percent(self, nums: List[int]) -> int:
+
+        length = input("Enter length of the array:")
+        counter = 0
+        max = - inf 
+        found = False
+        while not found:
+            if counter <= (length * 0.37):
+                current_value = input("Enter more values:")
+                if current_value > max:
+                    max = current_value
+            if counter > (length * 0.37):
+                current_value = input("Enter more values:")
+                if current_value > max:
+                    print(current_value + " is your best bet")
+                    return current_value
+                
+        return current_value
