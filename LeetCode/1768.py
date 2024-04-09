57956bc386613a93eb094b75742bc80a20b4f2f7
Kyumin Lee <2km4890@gmail.com>
2024-04-09 17:54:39 -0500

    added other's solution

--- LeetCode/1768.py
+++ LeetCode/1768.py
@@ -48,3 +48,32 @@ class Attempt2:
             for i in range(position, len(word2)):
                 result += word2[i]
             return result
+        
+'''
+this solution is very similar to my solution.
+but it utilizes array
+    appending stings as characters to an array is more efficient than appending strings
+and I was thinking about how to not check the length of word1 and word2
+this solution fixes it by
+    having i and j incremented together
+    and stop looping when either of them are at the end of the word
+    then append whatever is left in word1 or word2 from i or j to the end
+then "".join(result)
+    joins all the characters in the array
+    "" is the delimiter, meaning "" will be in between characters when appending
+'''
+class Others:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        result = []
+        
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word1[i])
+            i += 1
+            j += 1
+        result.append(word1[i:])
+        result.append(word1[j:])
+
+
+        return "".join(result)
