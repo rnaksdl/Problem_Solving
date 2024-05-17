b3a7b2cc8aba2cca30cc5c5850295aa31f3ce758
Kyumin Lee <2km4890@gmail.com>
2024-05-17 10:00:33 -0500

    working solution

--- LeetCode/274.py
+++ LeetCode/274.py
@@ -32,3 +32,16 @@ class Solution1:
 '''
 I could not come up with a working solution
 '''
+    
+class Solution2:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        
+        h_index = 0
+        for i, citation in enumerate(citations):
+            if citation >= i + 1:
+                h_index = i + 1
+            else:
+                break
+        
+        return h_index
