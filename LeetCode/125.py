7bd1403738165ba517a80886304180e48eda88e0
Kyumin Lee <2km4890@gmail.com>
2024-05-20 16:46:51 -0500

    adding 125

--- LeetCode/125.py
+++ LeetCode/125.py
@@ -21,3 +21,35 @@ class Solution:
 I need some hints...
 '''
 
+class Solution1:
+    def isPalindrome(self, s: str) -> bool:
+
+        # initialize an epmty array
+        filtered_chars = []
+
+        # iterate thru og array
+        for char in s:
+            
+            # filter only alphnumericals
+            if char.isalnum():
+
+                # convert upper to lower case and append
+                filtered_chars.append(char.lower())
+        
+        # initialize two pointers
+        left = 0
+        right = len(filtered_chars) - 1
+        
+        # iterate 
+        while left < right:
+
+            # array isn't palindrome
+            if filtered_chars[left] != filtered_chars[right]:
+                return False
+            
+            # moving poiters closer to middle
+            left += 1
+            right -= 1
+        
+        # is palindrome
+        return True
