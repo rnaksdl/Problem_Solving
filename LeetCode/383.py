4b3f62843f913afc334c7551538eafc7e73e8536
Kyumin Lee <79064939+rnaksdl@users.noreply.github.com>
2024-05-22 18:40:24 -0500

    383 sol1

--- LeetCode/383.py
+++ LeetCode/383.py
@@ -6,3 +6,36 @@ Given two strings ransomNote and magazine, return true if ransomNote can be cons
 Each letter in magazine can only be used once in ransomNote.
 '''
 
+class Solution1:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        counter = {}
+        
+        for char in magazine:
+            if char in counter:
+                counter[char] += 1
+            else:
+                counter[char] = 1
+                
+        for char in ransomNote:
+            
+            if char in counter:
+                counter[char] -= 1
+                if counter[char] == 0:
+                    del counter[char]
+            
+            if char not in counter:
+                return False
+            
+            return True
+
+'''
+Doesn't pass this test:
+Input
+"fihjjjjei"
+"hjibagacbhadfaefdjaeaebgi"
+Output
+true
+Expected
+false
+'''
