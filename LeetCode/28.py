99f2458f95e79612132d779727f25b697012fb46
Kyumin Lee <2km4890@gmail.com>
2024-05-23 15:33:04 -0500

    attempt 1

--- LeetCode/28.py
+++ LeetCode/28.py
@@ -5,3 +5,29 @@ Given two strings needle and haystack,
 return the index of the first occurrence of needle in haystack,
 or -1 if needle is not part of haystack.
 '''
+
+class Attempt1:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        start = -1
+        progress = 0
+        
+        for i, char in enumerate(haystack):
+            if char == needle[progress]:
+                progress += 1
+                if start == -1:
+                    start = i
+            
+            if progress == len(needle) - 1:
+                return start
+            
+            if char != needle[progress]:
+                progress = 0
+                start = -1
+                
+        return -1
+'''
+over compelecated for sure..
+'''
+
+
