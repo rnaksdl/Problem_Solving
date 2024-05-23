726965d92bd8aa0a3472fab1e4604f21131eeefc
Kyumin Lee <2km4890@gmail.com>
2024-05-23 17:56:51 -0500

    solution

--- LeetCode/28.py
+++ LeetCode/28.py
@@ -55,3 +55,59 @@ class Attempt2:
 ugh I should not reverse it,
 since I need to find the first occurrence
 '''
+
+class Attempt3:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        start = -1
+        progress = 0
+        
+        for i, char in enumerate(haystack):
+            if char == needle[progress]:
+                progress += 1
+                if start == -1:
+                    start = i
+                if progress == len(needle):
+                    return start
+                continue
+            
+            else:
+                start = -1
+                progress = 0
+        
+        return -1
+'''
+Input
+"mississippi"
+"issip"
+
+Output
+-1
+
+Expected
+4
+'''
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # Edge case: if needle is an empty string, return 0
+        if not needle:
+            return 0
+        
+        # Length of needle and haystack
+        n, m = len(haystack), len(needle)
+        
+        # Iterate over the haystack with a sliding window the size of the needle
+        for i in range(n - m + 1):
+            # Check if the substring of haystack starting at i matches needle
+            if haystack[i:i + m] == needle:
+                return i
+        
+        # If no match is found, return -1
+        return -1
+'''
+wow....
+really weird solution.
+I'm glad I attempted this question.
+This was very out of my knowledge.
+'''
