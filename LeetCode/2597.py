d9e5f8523cf8ae7bd783b89bebbf75710401f1ae
Kyumin Lee <2km4890@gmail.com>
2024-05-23 19:09:07 -0500

    attempt

--- LeetCode/2597.py
+++ LeetCode/2597.py
@@ -11,3 +11,26 @@ by deleting some (possibly none) elements from nums.
 Two subsets are different if and only if
 the chosen indices to delete are different.
 '''
+
+class Attempt:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        
+        sol = set()
+        
+        for n in nums:
+            sol.add(n)
+            for m in nums:
+                if abs(n - m) == k:
+                    continue
+                if n < m:
+                    sol.add((n, m))
+                elif m < n:
+                    sol.add((m, n))
+        
+        print(sol)
+        return len(sol)
+    
+'''
+only checks two numbers
+'''
+
