00e03c8d9d31266533ee45bdc6427de342a05913
Kyumin Lee <2km4890@gmail.com>
2024-05-24 10:29:15 -0500

    209 attempt

---
+++ LeetCode/209.py
@@ -0,0 +1,34 @@
+'''
+209. Minimum Size Subarray Sum
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray
+ whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+'''
+
+class Attempt:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        
+        L = 0
+        R = 0
+        total = nums[0]
+        length = len(nums) + 1
+        
+        while(L < len(nums) - 1 and R < len(nums)) - 1:
+            print(L)
+            print(R)
+            if total >= target:
+                if length > R - L + 1:
+                    length = R - L + 1
+                total -= nums[L]
+                L += 1
+                
+            if total <= target:
+                R += 1
+                total += nums[R]
+        
+        if length == len(nums) + 1:
+            return 0
+        
+        return length
+                
