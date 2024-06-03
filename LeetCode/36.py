21d95f0f304460c779b86e1c536386afac01c0a9
Kyumin Lee <2km4890@gmail.com>
2024-06-03 13:44:39 -0500

    adding solution

--- LeetCode/36.py
+++ LeetCode/36.py
@@ -10,3 +10,32 @@ Note:
 A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 Only the filled cells need to be validated according to the mentioned rules.
 '''
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        subgrids = [[set() for _  in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+
+                if num == '.':
+                    continue
+
+                if num in rows[i]:
+                    return False
+                rows[i].add(num)
+
+                if num in cols[j]:
+                    return False
+                cols[j].add(num)
+
+                if num in subgrids[i//3][j//3]:
+                    return False
+                subgrids[i//3][j//3].add(num)
+
+        return True
+
+  
