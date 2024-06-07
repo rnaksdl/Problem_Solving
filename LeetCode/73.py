76fcd2b18e3b7df228e2b7e1fc79931af730e3d7
Kyumin Lee <2km4890@gmail.com>
2024-06-07 06:32:02 -0500

    adding Attempt1

--- LeetCode/73.py
+++ LeetCode/73.py
@@ -5,3 +5,25 @@ Given an m x n integer matrix matrix, if an element is 0, set its entire row and
 
 You must do it in place.
 '''
+
+
+class Attempt1:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        rowWithZero = set()
+        colWithZero = set()
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    print(i)
+                    rowWithZero.add(i)
+                    colWithZero.add(j)
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i in rowWithZero and j in colWithZero:
+                    matrix[i][j] = 0
