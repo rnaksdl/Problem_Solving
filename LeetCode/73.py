e829d950d8204df368cad9f845e4868a6d5707f0
Kyumin Lee <2km4890@gmail.com>
2024-06-08 08:06:13 -0500

    adding optimized solution

--- LeetCode/73.py
+++ LeetCode/73.py
@@ -62,3 +62,40 @@ class Solution:
                 # if you're at a row or col with zero, change the value to 0
                 if i in rowWithZero or j in colWithZero:
                     matrix[i][j] = 0
+
+
+class Solution:
+    def setZeroes(self, matrix) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        firstRow = False
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        firstRow = True
+
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+    
+        if firstRow:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+'''
+only one extra variable to store info of whether firstRow shoud be zero or not
+others are stored in first row or col.
+[0][0] stores for the first col.
+'''
