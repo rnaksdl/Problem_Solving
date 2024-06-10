d9c19d07d79ba6b9c4a75b257257190360f3fcad
Kyumin Lee <2km4890@gmail.com>
2024-06-10 07:03:46 -0500

    adding solution

--- LeetCode/205.py
+++ LeetCode/205.py
@@ -54,3 +54,26 @@ class Attempt2:
 it should go both ways
 '''
 
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        sDict = {}
+        tDict = {}
+        
+        for i in range(len(s)):
+            if s[i] in sDict and sDict[s[i]] != t[i]:
+                return False
+            
+            if t[i] in tDict and tDict[t[i]] != s[i]:
+                return False
+
+            
+            sDict[s[i]] = t[i]
+            tDict[t[i]] = s[i]
+
+        return True
