b0a6e2ec16e78bde54a4aa6de3c7d4feb7dfb434
Kyumin Lee <2km4890@gmail.com>
2024-06-11 06:52:30 -0500

    adding attempt 1

--- LeetCode/290.py
+++ LeetCode/290.py
@@ -7,3 +7,21 @@ Here follow means a full match,
 such that there is a bijection between a letter in pattern
 and a non-empty word in s.
 '''
+
+class Attempt:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        counter = 0
+        word = ""
+        wordDict = {}
+
+        for char in s:
+            if char == " ":
+                counter += 1
+                
+                if pattern[counter] in wordDict and wordDict[pattern[counter]] != word:
+                    return False
+                wordDict[pattern[counter]] = word
+                word = ""
+        
+        return True
