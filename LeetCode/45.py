a3f539ce157da195a263ca14a41dcefabf880eba
Kyumin Lee <2km4890@gmail.com>
2024-06-14 00:27:09 -0500

    adding solution

--- LeetCode/45.py
+++ LeetCode/45.py
@@ -9,3 +9,19 @@ Each element nums[i] represents the maximum length of a forward jump from index 
 i + j < n
 Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
 '''
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        result = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            
+            l = r + 1
+            r = farthest
+            result += 1
+
+        return result
