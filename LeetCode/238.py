4bfe9e3ca9a90df8366e92b320fd2165c91f46d8
Kyumin Lee <2km4890@gmail.com>
2024-06-25 22:34:46 -0500

    adding opt sol

--- LeetCode/238.py
+++ LeetCode/238.py
@@ -44,7 +44,7 @@ class Attempt2:
         return result
     
 
-class Solution:
+class Solution1:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
         # init the array and val
@@ -74,3 +74,20 @@ class Solution:
 
         return result
 
+
+class Solution2:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
