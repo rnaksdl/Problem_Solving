66a4395c5a393a71cfd066b1787560c1cca4a789
Kyumin Lee <2km4890@gmail.com>
2024-06-29 23:13:49 -0500

    adding attempt1

--- LeetCode/20.py
+++ LeetCode/20.py
@@ -10,3 +10,28 @@ Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 '''
 
+class Attempt1:
+    def isValid(self, s: str) -> bool:
+
+        if len(s) % 2 != 0:
+            return False
+
+        stack = []
+
+        for char in s:
+            if char == "(" or char == "{" or char == "[":
+                stack.append(char)
+
+            if char == ")":
+                if stack.pop() != "(":
+                    return False
+            if char ==  "}":
+                if stack.pop() != "{":
+                    return False
+            if char ==  "]":
+                if stack.pop() != "[":
+                    return False
+        
+        if len(stack) != 0:
+            return False
+        return True
