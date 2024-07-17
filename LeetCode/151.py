1ba9578f63ea7520bf86784f077626d6a2701960
Kyumin Lee <2km4890@gmail.com>
2024-07-17 06:14:49 -1000

    adding comments

--- LeetCode/151.py
+++ LeetCode/151.py
@@ -13,25 +13,38 @@ Note that s may contain leading or trailing spaces or multiple spaces between tw
 class Solution1:
     def reverseWords(self, s: str) -> str:
         
+        # init word and words
         word = ""
         words = []
 
+        # iter s
         for c in s:
+
+            # if sees empty char
             if c == " ":
+                # if we have added anything to word
                 if word != "":
+                    # append word to words
                     words.append(word)
+                    # and reset word
                     word = ""
                 continue
             else:
+                # char not empty, add to word
                 word += c
         
+        # after iteration, if word isn't empty append to words and reset word
         if word != "":
             words.append(word)
             word = ""
 
+        # init result
         result = ""
+        # iter words backwords
         for i in range(len(words) - 1, -1, -1):
+            # add word to result
             result += words[i]
+            # add spaces in between words except for the last word (first, sice it's reversed)
             if i != 0:
                 result += " "
 
