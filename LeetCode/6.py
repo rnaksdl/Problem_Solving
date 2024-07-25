8686b646439c19a268579fd8fbb53634d4410e61
Kyumin Lee <2km4890@gmail.com>
2024-07-25 09:01:12 -0500

    adding comments

--- LeetCode/6.py
+++ LeetCode/6.py
@@ -16,13 +16,24 @@ string convert(string s, int numRows);
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
 
+        # edge case where it's just one character
         if numRows == 1: return s
 
+        # init res
         res = ""
+
+        # iter each row
         for r in range(numRows):
+            # 2:2, 3:4, 4:6
             increment = 2 * (numRows - 1)
+
+            # iter from r to len(s)
             for i in range(r, len(s), increment):
+
+                # add to result
                 res += s[i]
+
+                # for diagonals
                 if (r > 0 and r < numRows - 1 and i + increment - 2 * r < len(s)):
                     res += s[i + increment - 2 * r]
         
