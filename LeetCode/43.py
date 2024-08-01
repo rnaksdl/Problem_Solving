c6a8037b5f420f717360eefbf73f93557b538f03
Kyumin Lee <2km4890@gmail.com>
2024-07-31 20:13:58 -0500

    adding attempt1

--- LeetCode/43.py
+++ LeetCode/43.py
@@ -3,3 +3,18 @@
 
 Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 '''
+
+class Attempt1:
+    def trap(self, height: List[int]) -> int:
+        result = 0
+        for i in range(1, len(height) - 2):
+            left = height[i-1]
+            bottom = height[i]
+            right = height[i+1]
+
+            water = min(left, right) - bottom
+            if water >= 0:
+                result += water
+
+        return result
+            
