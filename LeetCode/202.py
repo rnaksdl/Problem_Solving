80b2e4863c8feee7db80209db37fcb6c6f83d0eb
Kyumin Lee <2km4890@gmail.com>
2024-08-05 09:49:34 -0500

    adding comments

--- LeetCode/202.py
+++ LeetCode/202.py
@@ -10,3 +10,29 @@ Repeat the process until the number equals 1 (where it will stay), or it loops e
 Those numbers for which this process ends in 1 are happy.
 Return true if n is a happy number, and false if not.
 '''
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        visited = set()
+
+        while True:
+            n_str = str(n)
+            result = 0
+            for c in n_str:
+                result += int(c) ** 2
+
+            if result == 1:
+                return True
+            elif result in visited:
+                return False
+            
+            visited.add(result)
+            n = result
+            
+            
+'''
+I didn't add "n = result" and got a wrong solution but I fixed it right away.
+But initially, I didin't want to use while true loop and was thinking about how to solve it without one.
+I failed to do so though.
+'''
